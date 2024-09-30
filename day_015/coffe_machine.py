784cce7e364f4e6708bb26e49ac8c76d2f5de7f3
Escutia <escutia_90@hotmail.com>
2024-09-30 12:57:49 -0600

    finished day 15

--- day_015/coffe_machine.py
+++ day_015/coffe_machine.py
@@ -1,3 +1,5 @@
+import sys
+
 coffe_choices = {
     "latte": {
         "water": 200,
@@ -29,22 +31,43 @@ machine_details = {
     "water": 10000,
     "milk": 5000,
     "coffee": 1000,
-    "money": {
-        "quarters": 100,
-        "dimes": 100,
-        "nickles" : 50,
-        "pennies": 50
-    }
+    "money": 0
 }
 
 isOn = True
 choice = ""
 
-def greet():
-    return input("What would you like? (espresso/latte/cappuccino):")
+def process_request(choice):
+    global machine_details
+    if machine_details["coffee"] > coffe_choices[choice]["coffee"] and machine_details["milk"] > coffe_choices[choice]["milk"] and machine_details["water"] > coffe_choices[choice]["water"]:
+        machine_details["coffee"] = machine_details["coffee"] - coffe_choices[choice]["coffee"]
+        machine_details["milk"] = machine_details["milk"] - coffe_choices[choice]["milk"]
+        machine_details["water"] = machine_details["water"] - coffe_choices[choice]["water"]
+        return True
+    else:
+        return False
 
 def count_coins(quarters, dimes, nickles, pennies):
     return (quarters * 0.25) + (dimes * 0.1) + (nickles * 0.05) + (pennies * 0.01)
+
+def processPayment(choice):
+    money = 0
+    quarters = int(input("How many quarters"))
+    dimes = int(input("How many quarters"))
+    nickles = int(input("How many quarters"))
+    pennies = int(input("How many quarters"))
+    money = count_coins(quarters, dimes, nickles, pennies)
+    if money > coffe_choices[choice]["money"]:
+        machine_details["money"] = machine_details["money"] + coffe_choices[choice]["money"]
+        return True
+    else:
+        return False
+
+
+def greet():
+    return input("What would you like? (espresso/latte/cappuccino):")
+
+
 
 
 
@@ -55,4 +78,15 @@ while isOn:
     choice = greet() 
     if choice == "off":
         turn_off()
+    elif choice == "report":
+        print(machine_details)
     else:
+        canProcess = process_request(choice)
+        if canProcess == True:
+            enoughMoney = processPayment(choice)
+            if enoughMoney == True:
+                print(f"Here is your {choice}, Enjoy!")
+            else:
+                print("Not enough Money, returning coins...")
+        else:
+            print(" Sorry, not enough ingredients to process your order")
